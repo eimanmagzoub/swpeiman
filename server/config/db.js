8fdc704ef5a614d2de89ac7cead40c401664bea5
eimanmagzoub <39648635+eimanmagzoub@users.noreply.github.com>
2018-05-30 17:25:01 +0300

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
-var connection = mongoose.connect('mongodb://localhost/mean_db');
-//var connection = mongoose.connect('mongodb://raya2018:raya2018@ds127994.mlab.com:27994/sudan_wp');
+//var connection = mongoose.connect('mongodb://localhost/mean_db');
+var connection = mongoose.connect('mongodb://raya2018:raya2018@ds127994.mlab.com:27994/sudan_wp');
  
 module.exports = connection;
 
